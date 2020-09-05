c8cfbe5aa634b4b601e1185198fbc86db39b2fbd
Kevin Huang <huangkevin96@gmail.com>
2020-09-05 14:07:22 -0400

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,7 @@ import time
 import unittest
 from django.test import LiveServerTestCase
 
-MAX_WAIT = 10
+MAX_WAIT = 3
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -41,6 +41,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
